29c3f9ccaae094da8217c5c262931fddc17aa3e4
MaxDzhura <105851617+MaxDzhura@users.noreply.github.com>
2022-05-24 12:36:14 +0300

    Add files via upload

---
+++ DZ_1.py
@@ -0,0 +1,18 @@
+# 1 дз
+
+print("Hello world")
+
+print("Hello\nmy\nworld")
+
+d = int(input())
+sh = int(input())
+print(d*sh)
+
+a= int(input())
+b= int(input())
+print(a+b,a*b,a-b,a/b)
+
+r= int(input())
+
+print("d:",(r*2),"l:" ,(r*2*3.14),"s:",(r*r*3.14))
+
